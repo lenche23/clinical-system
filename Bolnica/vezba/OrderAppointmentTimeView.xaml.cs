62cfe279f98f1804b1180888fda0a2bd9b299adb
lenche23 <aleksic.ra23.2018@uns.ac.rs>
2021-05-05 19:56:36 +0200

    validacija zakazivanje (preklapanje termina)

--- Bolnica/vezba/OrderAppointmentTimeView.xaml.cs
+++ Bolnica/vezba/OrderAppointmentTimeView.xaml.cs
@@ -48,8 +48,6 @@ namespace vezba
                 int id = storage.generateNextId();
                 Appointment a = new Appointment(selectedDoctor, dateTimeFinal, patient);
                 a.AppointentId = id;
-                storage.Save(a);
-                PatientView.Apps.Add(a);
 
                 int diff = (selectedDate - DateTime.Now.Date).Days;
                 if (diff <= 0)
@@ -58,14 +56,66 @@ namespace vezba
                     this.Close();
                 }
                 else {
-                    MessageBox.Show("Uspešno ste zakazali pregled.");
-                    this.Close();
+                    if (GetOverlapingAppoinments(a).Count == 0)
+                    {
+                        Boolean b = storage.Save(a);
+                        if (b)
+                        {
+                            PatientView.Apps.Add(a);
+                        }
+                        MessageBox.Show("Uspešno ste zakazali pregled.");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Termin je zauzet! Izaberite drugo vreme.");
+                    }
                 }                
             }
             else
             {
                 MessageBox.Show("Molimo Vas popunite sva potrebna polja!");
             }
+        }
+
+        private List<Appointment> GetOverlapingAppoinments(Appointment appointment)
+        {
+            AppointmentStorage aps = new AppointmentStorage();
+            List<Appointment> appointments = aps.GetAll();
+            List<Appointment> overlaping = new List<Appointment>();
+            for (int i = 0; i < appointments.Count; i++)
+            {
+                if (this.DoShareResources(appointment, appointments[i]))
+                {
+                    overlaping.Add(appointments[i]);
+                }
+            }
+            return overlaping;
+        }
+
+        private Boolean DoShareResources(Appointment a1, Appointment a2)
+        {
+            if (a1.Doctor.Jmbg.Equals(a2.Doctor.Jmbg) || a1.Patient.Jmbg.Equals(a2.Patient.Jmbg))
+            {
+                DateTime beginning1 = a1.StartTime;
+                DateTime end1 = a1.StartTime.AddMinutes(a1.DurationInMunutes);
+                DateTime beginning2 = a2.StartTime;
+                DateTime end2 = a2.StartTime.AddMinutes(a2.DurationInMunutes);
+                if (DateTime.Compare(end2, beginning1) <= 0) //drugi zavrsava pre pocetka prvog
+                {
+                    return false;
+                }
+                else if (DateTime.Compare(end1, beginning2) <= 0) //prvi zavrsava pre pocetka drugog
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+
         }
     }
 }
