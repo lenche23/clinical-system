6a75fae87d91dca79d8981357c7cbf4d49a784be
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2021-05-19 15:06:35 +0200

    User korigovan

---
+++ Bolnica/vezba/Repository/UserStorage.cs
@@ -0,0 +1,42 @@
+// File:    UserStorage.cs
+// Author:  bogdana
+// Created: četvrtak, 25. mart 2021. 19.13.50
+// Purpose: Definition of Class UserStorage
+
+using System;
+using System.Collections.Generic;
+using Model;
+
+namespace Repository
+{
+   public class UserStorage1
+   {
+      public List<User> GetAll()
+      {
+         throw new NotImplementedException();
+      }
+      
+      public void Save(User user)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public Boolean Update(User user)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public User GetOne(String username)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public Boolean Delete(String username)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public String fileName;
+   
+   }
+}
