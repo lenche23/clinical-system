795cf75bcc6a7156672c6b834f48aaaa8380b5fa
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2021-05-30 17:06:50 +0200

    Izmene interfejsa

--- Bolnica/vezba/Repository/IEquipmentRepository.cs
+++ Bolnica/vezba/Repository/IEquipmentRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace vezba.Repository
 {
-    interface IEquipmentRepository
+    interface IEquipmentRepository: IGenericRepository<Equipment, int>
     {
     }
 }
--- Bolnica/vezba/Repository/IRoomInventoryRepository.cs
+++ Bolnica/vezba/Repository/IRoomInventoryRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace vezba.Repository
 {
-    interface IRoomInventoryRepository
+    interface IRoomInventoryRepository: IGenericRepository<RoomInventory, int>
     {
     }
 }
