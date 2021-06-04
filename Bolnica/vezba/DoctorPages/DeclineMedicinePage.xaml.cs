f3e31f97ac67192b5ed7419703b158c0d834d9ee
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-06-04 15:21:44 +0200

    validacija za odbijanje lekova

--- Bolnica/vezba/DoctorPages/DeclineMedicinePage.xaml.cs
+++ Bolnica/vezba/DoctorPages/DeclineMedicinePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using Model;
 using Service;
@@ -8,24 +10,54 @@ namespace vezba.DoctorPages
     /// <summary>
     /// Interaction logic for DeclineMedicinePage.xaml
     /// </summary>
-    public partial class DeclineMedicinePage : Page
+    public partial class DeclineMedicinePage : Page, INotifyPropertyChanged
     {
         public Medicine Medicine { get; set; }
 
         private readonly DoctorView _doctorView;
 
         private readonly MedicinePageView _medicinePageView;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private String _description;
+        public String Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                if (value != _description)
+                {
+                    _description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
         public DeclineMedicinePage(Medicine medicine, DoctorView doctorView, MedicinePageView medicinePageView)
         {
             InitializeComponent();
             Medicine = medicine;
-            DataContext = Medicine;
+            DataContext = this;
             _doctorView = doctorView;
             _medicinePageView = medicinePageView;
         }
 
         private void OkButtonClick(object sender, RoutedEventArgs e)
         {
+            if(!ValidateEntries())
+                return;
             var medicineService = new MedicineService();
 
             var description = DescriptionTB.Text;
@@ -47,6 +79,14 @@ namespace vezba.DoctorPages
         private void CancelButtonClick(object sender, RoutedEventArgs e)
         {
             _doctorView.Main.GoBack();
+        }
+
+        private Boolean ValidateEntries()
+        {
+            DescriptionTB.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            if (Validation.GetHasError(DescriptionTB))
+                return false;
+            return true;
         }
     }
 }
